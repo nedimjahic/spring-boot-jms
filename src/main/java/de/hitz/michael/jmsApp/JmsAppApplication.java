f3089afd6f7dfdaeac8cee7f3dd592a599dc043e
Nedim Jahic <nedim.jahic.extern@bdr.de>
2023-11-10 00:53:31 +0100

    Initial commit.

---
+++ src/main/java/de/hitz/michael/jmsApp/JmsAppApplication.java
@@ -0,0 +1,13 @@
+package de.hitz.michael.jmsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hitz/michael/jmsApp/JmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hitz.michael.jmsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
